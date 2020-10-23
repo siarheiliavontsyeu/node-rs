9da88fa7001415c214a6b3c12dad465e60b56d56
Siarhei Liavontsyeu <leontevsi@gmail.com>
2020-10-23 17:01:28 +0300

    feat: add mongoose, implement MongoDB connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
-const { PORT } = require('./common/config');
+const mongoose = require('mongoose');
+const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
 const logger = require('./common/logging');
 const app = require('./app');
 
-app.listen(PORT, () =>
-  logger.info(`App is running on http://localhost:${PORT}`)
-);
+mongoose.connect(MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+mongoose.connection
+  .on('error', () => logger.error('MongoDB connection fail:'))
+  .once('open', () => {
+    logger.info('Successfully connect to MongoDB');
+    app.listen(PORT, () =>
+      logger.info(`App is running on http://localhost:${PORT}`)
+    );
+  });
