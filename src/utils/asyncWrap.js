cd4d14a79ba3d27a6931913a034c10eb6e04aa72
Siarhei Liavontsyeu <leontevsi@gmail.com>
2020-10-23 10:58:47 +0300

    refactor: delete asyncWrap.js unusing function

--- src/utils/asyncWrap.js
+++
@@ -1,9 +0,0 @@
-const asyncWrap = callback => async (req, res, next) => {
-  try {
-    return await callback(req, res, next);
-  } catch (err) {
-    return next(err);
-  }
-};
-
-module.exports = asyncWrap;
